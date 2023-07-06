13c820035fc8b120ce504ec7520eefce8aac3e7b
Joanne Carpenter <car22047@byui.edu>
2023-07-06 19:48:26 -0400

    updates for W05 Team Activity

---
+++ scripts/links.js
@@ -0,0 +1,19 @@
+const baseURL = "https://joannecarpenter.github.io/wdd230/";
+const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+
+async function getLinks(url) {
+    try {   
+        const response = await fetch(url);
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+            //displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    }   catch (error) {
+            console.log(error);
+    }
+}
+
+getLinks(linksURL);
