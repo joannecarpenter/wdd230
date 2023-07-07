4b6ef0dbf3325512c15509f3b2f68e338b963150
Joanne Carpenter <car22047@byui.edu>
2023-07-06 21:44:59 -0400

    W05 Team Activity update

--- scripts/links.js
+++ scripts/links.js
@@ -7,7 +7,7 @@ async function getLinks(url) {
         if (response.ok){
             const data = await response.json();
             console.log(data);
-            //displayResults(data);
+            //displayLinks(data);
         } else {
             throw Error(await response.text());
         }
@@ -17,3 +17,21 @@ async function getLinks(url) {
 }
 
 getLinks(linksURL);
+
+const displayLinks = (weeks) => {
+    weeks.forEach((item) => {
+        let listItem = document.createElement('li');
+        let week = item.week;
+        listItem.textContent = week;
+        
+        links.forEach((link) => {
+            let linkAnchor = document.createElement('a');
+            let url = link.url;
+            let title = link.title;
+            linkAnchor.setAttribute('href', url);
+            linkAnchor.setAttribute('title', title);
+            listItem.appendChild(linkAnchor);
+        });
+        
+    });
+}
