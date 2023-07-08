6de5392854ceae4be90d25e196c62eecbd76ba8b
Joanne Carpenter <car22047@byui.edu>
2023-07-08 09:55:51 -0400

    update to member.json data format

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,16 +1,16 @@
 const baseURL = "https://joannecarpenter.github.io/wdd230/";
 const membersURL = "https://joannecarpenter.github.io/wdd230/chamber/data/members.json";
 
-// Create the <ul>
-const list = document.getElementById('list');
+// Link to table in html
+const businesses = document.querySelector('#businesses');
 
 async function getMembers(url) {
     try {   
         const response = await fetch(url);
         if (response.ok){
             const data = await response.json();
-            console.table(data.members);
-            // displayLinks(data.members);
+            console.table(data.members);  //DEBUG - REMOVE AFTER FUNCTIONALITY CONFIRMED
+            displayMembers(data.members);
         } else {
             throw Error(await response.text());
         }
@@ -20,3 +20,37 @@ async function getMembers(url) {
 }
 
 getMembers(membersURL);
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        // Create table row for appending all table data items
+        let row = document.createElement('tr');
+
+        // Create new table data box for each piece of info
+        // Insert appropriate json file data to each table box
+        // Append each <td> to the <tr>
+        let name = document.createElement('td');
+        name.textContent = member.name;
+        row.appendChild(name);
+
+        let address = document.createElement('td');
+        address.textContent = member.address;
+        row.appendChild(address);
+
+        let phone = document.createElement('td');
+        phone.textContent = member.phone;
+        row.appendChild(phone);
+
+        let url = document.createElement('td');
+        url.textContent = member.url;
+        row.appendChild(url);
+
+        let industryInfo = document.createElement('td');
+        industryInfo.textContent = member.icon + ` ${member.industry}`;
+        row.appendChild(industryInfo);
+
+        // Append tr to the table
+        businesses.appendChild(row);
+   
+    });
+}
