135e923d3b98c45d212a6f3467e67a2ed8bb5fe6
Joanne Carpenter <car22047@byui.edu>
2023-07-07 21:58:19 -0400

    completed W05 Team Activity

--- scripts/links.js
+++ scripts/links.js
@@ -1,13 +1,16 @@
 const baseURL = "https://joannecarpenter.github.io/wdd230/";
 const linksURL = "https://joannecarpenter.github.io/wdd230/data/links.json";
 
+// Create the <ul>
+const list = document.getElementById('list');
+
 async function getLinks(url) {
     try {   
         const response = await fetch(url);
         if (response.ok){
             const data = await response.json();
-            console.log(data);
-            //displayLinks(data);
+            console.table(data.weeks);
+            displayLinks(data.weeks);
         } else {
             throw Error(await response.text());
         }
@@ -20,18 +23,37 @@ getLinks(linksURL);
 
 const displayLinks = (weeks) => {
     weeks.forEach((item) => {
+        // Create the <li>
         let listItem = document.createElement('li');
-        let week = item.week;
-        listItem.textContent = week;
+        // Set the textContent for the <li>
+        listItem.textContent = `${item['week']}: `;
         
-        links.forEach((link) => {
-            let linkAnchor = document.createElement('a');
-            let url = link.url;
-            let title = link.title;
-            linkAnchor.setAttribute('href', url);
-            linkAnchor.setAttribute('title', title);
-            listItem.appendChild(linkAnchor);
-        });
+        // Make a loop here to go through each link in that array of links
+        // until the index equals the length of the links array minus 1
+        let loopTime = item['links'].length;
+        let i = 0;
+        do {
+            // Create the <a>
+            let anchor = document.createElement('a');
+            // let link = linksArray[i].url;
+            let link = item.links[i].url;
+            let linkDescription = item.links[i].title;   
         
+            // continue code to create the anchor content
+            // Set the href attribute for the <a>
+            anchor.setAttribute('href', link);
+            // Set the textContent for the <a>
+            anchor.textContent = linkDescription;
+            
+            // Append the <a> to the <li>
+            listItem.appendChild(anchor);
+            // Append the <li> to the <ul>
+            list.appendChild(listItem);
+
+        
+
+            // Increment i
+            i++;
+        } while (i < loopTime);
     });
 }
