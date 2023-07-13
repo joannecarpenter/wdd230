4f71061f86858f1b3288c0ce6a788d4172a86329
Joanne Carpenter <car22047@byui.edu>
2023-07-12 23:38:19 -0400

    spotlight functionality completed

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -1,12 +1,6 @@
-const name1 = document.querySelector('#name1');
-const info1 = document.querySelector('#info1');
-const web1 = document.querySelector('#web1');
-const name2 = document.querySelector('#name2');
-const info2 = document.querySelector('#info2');
-const web2 = document.querySelector('#web2');
-const name3 = document.querySelector('#name3');
-const info3 = document.querySelector('#info3');
-const web3 = document.querySelector('#web3');
+const spotlightContainer = document.querySelector('#spot-box');
+// Create an array to store the possible spotlights
+const spotlights = [];
 
 const baseURL = "https://joannecarpenter.github.io/wdd230/";
 const membersURL = "https://joannecarpenter.github.io/wdd230/chamber/data/members.json";
@@ -17,9 +11,10 @@ async function getMembers(url) {
         const response = await fetch(url);
         if (response.ok){
             const data = await response.json();
-            console.log(data);  //DEBUG - REMOVE AFTER FUNCTIONALITY CONFIRMED
-            console.table(data.members);
-            //displaySpotlights(data.members);
+            //console.log(data);  //DEBUG - REMOVE AFTER FUNCTIONALITY CONFIRMED
+            //console.table(data.members);
+            filterMembers(data.members);
+            displaySpotlights();
         } else {
             throw Error(await response.text());
         }
@@ -27,12 +22,10 @@ async function getMembers(url) {
             console.log(error);
     }
 }
-
 getMembers(membersURL);
-    
-const displaySpotlights = (members) => {
-    // Create an array to store the possible spotlights
-    const spotlights = [];
+
+// Filter members to silver and gold levels only, and add them to the spotlights array 
+const filterMembers = (members) => {
     members.forEach((member) => {
         let name = member.name;
         let membership = member.memLevel;
@@ -43,10 +36,53 @@ const displaySpotlights = (members) => {
         if (membership === 'Silver' || membership === 'Gold') {
             let memberDetails = `${name}, ${info}, ${website}`;
             spotlights.push(memberDetails);
-            console.log(spotlights);
-        }
-                     
-    })
+            //console.log(spotlights);
+        }                    
+    });
+}
 
-    // Randomly select 3 members from the spotlights array, and add them to the page
+
+function GetRandomMember(){
+    let randomIndex = Math.floor(Math.random() * spotlights.length) + 1;
+    return spotlights[randomIndex];
+};
+
+// Randomly select 3 members from the spotlights array, and add them to the page
+function displaySpotlights(){
+    let count = 1;
+    do {
+        let randomMember = GetRandomMember();
+        //console.log(randomMember);
+
+        // Split the member string into pieces to be placed in the document
+        let memberParts = randomMember.split(', ');
+        let name = memberParts[0];
+        let info = memberParts[1];
+        let website = memberParts[2];
+
+        // Create the elements and content for company profile
+        let profile = document.createElement('div');
+        profile.setAttribute('class', 'profile');
+
+        let companyName = document.createElement('h3');
+        companyName.textContent = name;
+        profile.appendChild(companyName);
+
+        let companyInfo = document.createElement('p');
+        companyInfo.innerHTML = info;
+        profile.appendChild(companyInfo);
+
+        let companyWeb = document.createElement('a');
+        companyWeb.setAttribute('href', website);
+        companyWeb.textContent = 'Website';
+        profile.appendChild(companyWeb);
+
+        // Add company profile to spotlight box
+        spotlightContainer.appendChild(profile);
+        
+        // Increment count to ensure this only gets repeated 3 times
+        count ++;
+
+    } while (count < 4);
+
 }
